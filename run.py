2703d96fc1691455223f0031d738378feffa1d21
Steph B <SMBoatman@live.co.uk>
2022-05-17 10:20:02 +0000

    Add owner_data, feedback and main functions, with print statements and input for users

--- run.py
+++ run.py
@@ -1,3 +1,35 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def owner_data():
+    """
+    Get owner's information input from user.
+    """
+    print("Please tell us your name.")
+    owner = input("Name: \n")
+    print("Please tell us your pet name.")
+    pet = input("Pet name: \n")
+
+
+def feedback():
+    """
+    Feedback input from user.
+    User chooses an input 0-5, which must be integers.
+    The loop will repeat if user enters an invalid input.
+    """
+    print("Please answer the below questions.")
+    print("Your answer should be a figure between 0-5")
+    print("0 = Awful; 5 = Absolutely fantastic.\n")
+
+    print("1. Would you recommend Pawsitively Perfect to friends?")
+    data_1 = input("Enter answer here:\n")
+
+feedback()
+
+def main():
+    """
+    Run all program functions
+    """
+    owner_data()
+
+
+print("Thank you for choosing Pawsitively Perfect Pet Care.")
+print("We would love to hear your feedback about our services.")
+main()
